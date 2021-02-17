a8bdab6137bd522deae38bbabb448c23fca740ff
George Papagapitos <george.g.papagapitos@gmail.com>
2021-02-17 16:28:55 -0600

    PartyLeader component complete

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,6 +6,7 @@ import Footer from '../Footer/Footer';
 import GuestList from '../GuestList/GuestList';
 import DinnerSupplies from '../DinnerSupplies/DinnerSupplies';
 import GuestForm from '../GuestForm/GuestForm';
+import PartyLeader from '../PartyLeader/PartyLeader';
 
 function App() {
   let [guestList, setGuestList] = useState([]);
@@ -58,9 +59,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <h2>Party Leader</h2>
-      {guestList[0] && <h3>{guestList[0].name}</h3>}
-      <h2>Add a new guest</h2>
+      <PartyLeader leader={guestList[0]} />
       <GuestForm
         newGuestName={newGuestName}
         setNewGuestName={setNewGuestName}
