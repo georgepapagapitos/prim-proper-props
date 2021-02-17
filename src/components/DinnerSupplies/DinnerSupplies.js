67ba1ad7aeb617e906abdfa32126807ee8cce4d5
George Papagapitos <george.g.papagapitos@gmail.com>
2021-02-17 17:38:32 -0600

    silverware component working...i think...

--- src/components/DinnerSupplies/DinnerSupplies.js
+++ src/components/DinnerSupplies/DinnerSupplies.js
@@ -4,9 +4,7 @@ function DinnerSupplies({ guestList, count }) {
   return (
     <>
       <h2>Dinner Supplies</h2>
-      <SilverWare name="Spoons" count={count} guestList={guestList} />
-      <SilverWare name="Forks" count={count} guestList={guestList} />
-      <SilverWare name="Knives" count={count} guestList={guestList} />
+      <SilverWare count={count} guestList={guestList} />
     </>
   );
 }
